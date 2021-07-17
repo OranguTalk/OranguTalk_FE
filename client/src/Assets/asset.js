a447ea7df3471d4dec915efa09401017feb1c1dc
qhahd78 <umuj0708@likelion.org>
2021-07-17 15:40:16 +0900

    Setting :: 초기 세팅

---
+++ client/src/Assets/asset.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function asset() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default asset
---
+++ client/src/Pages/Main2/Main2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main2() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Main2
