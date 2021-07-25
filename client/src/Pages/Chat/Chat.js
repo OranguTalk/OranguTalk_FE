1bcd0355cd9e9fe11f008de1f72799f679ea848e
qhahd78 <umuj0708@likelion.org>
2021-07-25 20:01:28 +0900

    Develop :: socket setting

--- client/src/Pages/Chat/Chat.js
+++ client/src/Pages/Chat/Chat.js
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../../Components/Chat/Header';
 import styled from 'styled-components';
 import Chatroom from '../../Components/Chat/Chatroom';
 import Chatinput from '../../Components/Chat/Chatinput';
-import socketIOClient from 'socket.io-client';
+import { useLocation } from 'react-router-dom';
 
 const Container = styled.div`
   width: 100%;
 `;
 
-const Chat = ({ match }) => {
-  const [Socket, setSocket] = useState(match.params.socket);
-  console.log(Socket);
-
+const Chat = () => {
+  const location = useLocation();
+  console.log(location.socket);
   return (
     <Container>
       <Header roomname={'오랑이'} />
