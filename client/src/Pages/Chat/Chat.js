6e3c4372c37525cb12b9ae0a581581b2275b911e
qhahd78 <umuj0708@likelion.org>
2021-07-27 23:01:10 +0900

    Develop :: ChatDetail

--- client/src/Pages/Chat/Chat.js
+++ client/src/Pages/Chat/Chat.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../Components/Chat/Header';
 import styled from 'styled-components';
 import Chatroom from '../../Components/Chat/Chatroom';
 import Chatinput from '../../Components/Chat/Chatinput';
 import { useLocation } from 'react-router-dom';
+import { ChatList, GetUserListInRoom } from '../../Api/User';
 
 const Container = styled.div`
   width: 100%;
 `;
 
-const Chat = () => {
+const Chat = ({ match }) => {
   const location = useLocation();
   console.log(location.socket);
+  const [ChatListInfo, setChatListInfo] = useState([]);
+  const room_id = match.params.id;
+  // 채팅방 정보, 내역 조회 api 실행
+  useEffect(() => {
+    const fetchChatList = async () => {
+      try {
+        const chatList = (await ChatList(room_id)).data;
+        setChatListInfo(chatList);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchChatList();
+  }, []);
   return (
     <Container>
-      <Header roomname={'오랑이'} />
+      <Header room_id={room_id} room_info={ChatListInfo} />
       <Chatroom />
       <Chatinput />
     </Container>
