10b9a12ff03b0f93ed679e2be46554e569bb1939
qhahd78 <umuj0708@likelion.org>
2021-07-29 00:33:02 +0900

    Develop, Publishing :: ChatNum setting, UI edit

--- client/src/Recoil/CreateRoom.js
+++ client/src/Recoil/CreateRoom.js
@@ -1,6 +1,13 @@
 import { atom } from 'recoil';
 
+// 룸생성시 룸 제목
 export const CreateRoomState = atom({
   key: 'CreateRoomState',
+  default: '',
+});
+
+// 룸 총 num 값
+export const RoomNumState = atom({
+  key: 'RoomNumState',
   default: '',
 });
